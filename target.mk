d20f55022bc521c6f1ce58213cf84e2fd28d8424
Ryan Hartlage <ryanplusplus@gmail.com>
2021-04-04 20:26:56 -0400

    Cleanup

--- target.mk
+++ target.mk
@@ -18,26 +18,12 @@ DEFINES := \
   STM32F051x8 \
   HSE_VALUE=8000000 \
 
-ifeq ($(USE_SYSTEM_VIEW),Y)
-DEFINES += \
-  USE_SYSTEM_VIEW \
-
-endif
-
 include tools/defaults.mk
 
 CPPFLAGS += \
   -Wno-unused-parameter \
 
-ifeq ($(USE_SYSTEM_VIEW),Y)
-FREERTOS := lib/patched/FreeRTOS-Kernel
-else
-FREERTOS := lib/FreeRTOS-Kernel
-endif
-
 SRC_DIRS := \
-  $(FREERTOS) \
-  $(FREERTOS)/portable/GCC/ARM_CM0 \
   src \
   src/hardware \
 
@@ -45,34 +31,11 @@ SRC_FILES := \
   lib/stm32cube/CMSIS/STM32F0xx/src/system_stm32f0xx.c \
 
 INC_DIRS := \
-  $(FREERTOS)/include \
   lib/stm32cube/CMSIS/ARM/inc \
   lib/stm32cube/CMSIS/STM32F0xx/inc \
   lib/stm32cube/HAL/STM32F0xx/inc \
 
-ifeq ($(USE_SYSTEM_VIEW),Y)
-SRC_FILES += \
-  lib/SystemView/Sample/NoOS/Config/Cortex-M0/SEGGER_SYSVIEW_Config_NoOS_CM0.c \
-  lib/SystemView/SEGGER/SEGGER_RTT.c \
-  lib/SystemView/SEGGER/SEGGER_SYSVIEW.c \
-
-SRC_DIRS += \
-  lib/SystemView/Sample/FreeRTOSV10 \
-
-INC_DIRS += \
-  $(FREERTOS)/include \
-  lib/stm32cube/CMSIS/ARM/inc \
-  lib/stm32cube/CMSIS/STM32F0xx/inc \
-  lib/stm32cube/HAL/STM32F0xx/inc \
-  lib/SystemView/SEGGER \
-  src/system_view \
-
-endif
-
-IGNORE := $(shell $(MAKE) --no-print-directory -f patch.mk)
-
-BUILD_DEPS += patch.mk
-
+include freertos.mk
 include lib_tiny.mk
 
 include tools/tools.mk
